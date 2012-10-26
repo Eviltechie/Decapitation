e00c48d6ef44fae9cb2f5c5296827aad804ca908
Ivan Panarusky <eviltechie@joe.to>
2012-10-26 16:38:28 -0400

    First

---
+++ src/main/java/to/joe/decapitation/Decapitation.java
@@ -0,0 +1,8 @@
+package to.joe.decapitation;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Decapitation extends JavaPlugin implements Listener {
+	
+}
